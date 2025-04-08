3ee130a106be5dddac3d172b30501a6a9e0ff1c9
Johannes Plunien <johannes@plunien.com>
2025-04-08 10:57:55 +0200

    Reduce sample text

--- Example1/TextViewCrash/ViewController.swift
+++ Example1/TextViewCrash/ViewController.swift
@@ -8,21 +8,7 @@ final class ViewController: UIViewController {
     @IBAction
     func onTouchUp(_ sender: Any) {
 
-        let text = #"""
-        Wir sind vor 1 Woche umgezogen und ich habe mir extra hierfür (habe dann endlich ein kleines aber feines Büro für mich) am 08.01.25 einen nagelneuen iMac M4, 10‑Core CPU, 10-Core GPU, 16 GB gemeinsamer Arbeitsspeicher und 256 GB Speicherplatz inkl. vier Thunderbolt 4 Anschlüssen und Gigabit Ethernet für mein neues HomeOffice als Privatrechner gekauft.
-        
-        Mein Schreibtisch im neuen Büro ist 160cm breit. Da passt der iMac optisch total gut hin..... dachte ich.... Man muss wissen, dass ich ab März einen neuen Arbeitgeber habe und der hat mir nun eröffnet, dass er mir für mein HomeOffice zu beruflichen Zwecken einen Laptop mit 2 x 24-Zoll Monitoren liefert.... merkt ihr was....!?!?
-        
-        Das passt nicht alles auf den Schreibtisch. Somit werde ich privat auf ein Macbook umstiegen müssen und mich nach wenigen Tagen (der Rechner ist erst seit 1 Woche in Betrieb) vom neuen iMac trennen müssen
-        
-        Der iMac kommt wie auf dem Bild. Die Tastatur sowie die Maus sind völlig unbenutzt und noch original verpackt. Das Ganze wir im Originalkarton geliefert.
-        
-        Die Rechnung kann ich dann auch, wegen der Garantie, mitliefern.
-        
-        Die weiteren technischen Daten bitte ich apple-Website zu entnehmen.
-        
-        Ich werde den Rechner per DHL mit zusätzlicher Transportversicherung (versichert bis 2.500 Euro anstatt 500 Euro) versenden. Die Versandkosten (über 10 kg) trägt der Käufer, die zusätzliche Transportversicherung trage ich.
-        """#
+        let text = "ffi"
 
         let paragraphStyle = NSMutableParagraphStyle()
         paragraphStyle.lineHeightMultiple = 1.2
--- Example2/TextViewCrash/ViewController.swift
+++ Example2/TextViewCrash/ViewController.swift
@@ -15,87 +15,9 @@ final class ViewController: UITableViewController {
             NSAttributedString.Key.font: UIFont(name: "KleinanzeigenRebondText-Regular", size: 14.0)!,
             NSAttributedString.Key.paragraphStyle: paragraphStyle
         ]
+        let text = "ffi"
         let attributedText = NSAttributedString(string: text, attributes: attributes)
 
         textView.attributedText = attributedText
     }
 }
-
-let text = #"""
-Als offizieller HSK-Partner erhalten Sie bei uns das komplette Sortiment. Fragen Sie einfach an!
-
-"Fragen Sie bei uns die gewünschten Artikel mit der HSK-Artikelnummer an, und wir erstellen Ihnen ein individuelles Angebot."
-
-ÄSTHETISCH, HOCHWERTIG SOWIE ÄUSSERST FUNKTIONAL
-
-MARMOR-POLYMER DUSCHWANNEN IN STEINOPTIK
-
-Die Steinoptik der Marmor-Polymer Duschwannen bringt einen edlen Look in die Dusche. Besonders vorteilhaft: Die 4 cm starke Wanne ist nicht nur montagefreundlich bei ebenerdigem Einbau oder auf höhenverstellbaren Füßen, sondern auch besonders rutschsicher für die Benutzer.
-
-PRODUKTINFORMATION
-
-EIGENSCHAFTEN
-
-•    Strukturierte Oberfläche
-•    Dezenter Ablauf mit gleicher Beschaffenheit wie Duschwannen
-•    Massive Ausführung
-•    Wärmespeichernd
-•    Hautsympathisch
-•    Pflegeleicht
-•    Bodengleicher Einbau
-•    Auf höhenverstellbaren Füßen
-•    Direkt auf den Boden
-•    Stabile Montage durch bewährte Höhenverstelltechnik
-•    Bequemes Handling durch Montage von oben
-•    Keine Bohrung in der Wand - Befestigung am Boden
-•    Besonders geeignet in der Sanierung unebener Böden
-•    Mittenabstützung mit EasyLift Wannenfuß
-•    DIE PLUSPUNKTE
-+ Für Acryl & Marmor-Polymer Duschwannen + Flexibel für nahezu alle Einbausituationen geeignet + Dauerhaft zuverlässig wasserdicht + Professionelle Abdichtung + Besonders elastisch und dehnbar + Komfortable Montage
-
-ANTISLIP – FÜR ERHÖHTE TRITTSICHERHEIT
-
-OPTIONAL FÜR ALLE DUSCHWANNEN
-
-Alle Duschwannen können werkseitig mit einer Schutzschicht für mehr Trittsicherheit ausgerüstet werden. Diese weltweit einzig zertifizierte wasserbasierte 2-Komponenten Beschichtung entspricht der Rutschsicherheitsklasse C nach DIN 51097 und ist nach ISO 22196 eine antibakteriell aktive Oberfläche. Neben einem sicheren Stand besticht die AntiSlip Beschichtung ebenfalls durch mühelose Pflege. Durch die Pflegeversicherung ist außerdem bei vorhandener Pflegegradeinstufung eine Kostenübernahme von bis zu 4000€ möglich. Ein weiterer Vorteil: Mit dem separat erhältlichen Remover kann die Beschichtung jederzeit rückstandslos entfernt werden.
-
-QUADRAT
-
-•    Wannentiefe ca. 0 - 1,5 cm
-
-Verfügbare Maße:
-
-90 x 90 x 4,0 cm
-
-100 x 100 x 4,0 cm
-
-120 x 120 x 4,0 cm
-
-RECHTECK
-
-•    Wannentiefe ca. 0 - 1,5 cm
-
-Verfügbare Maße:
-
-75 x 100 x 4,0 cm
-
-90 x 100 x 4,0 cm
-
-75 x 120 x 4,0 cm
-
-90 x 120 x 4,0 cm
-
-75 x 140 x 4,0 cm
-
-90 x 140 x 4,0 cm
-
-80 x 100 x 4,0 cm
-
-100 x 120 x 4,0 cm
-
-80 x 120 x 4,0 cm
-
-100 x 140 x 4,0 cm
-
-80 x 140 x 4,0cm
-"""#
